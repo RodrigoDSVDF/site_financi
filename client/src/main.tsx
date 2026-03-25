febe9f2fb816cacfc9de3e624fc2270e0bd89556
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2026-03-24 22:57:31 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Verifique se o App está na mesma pasta
+import App from './App';
 import './index.css';
+
+// Garantir que o TypeScript entenda o objeto fbq do Facebook
+declare global {
+  interface Window {
+    fbq: any;
+    ttq: any;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
